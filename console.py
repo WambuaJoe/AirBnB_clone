d13902766c611f18f0146fa4350ecaf1a1cb796e
WambuaJoe <kaytujohn254@gmail.com>
2023-11-14 19:46:13 +0300

    create entry point for the command interpreter

---
+++ console.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Console Module"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Console class"""
+    prompt = "(hbnb) "
+    def do_quit(self, args):
+        """Quit command that exits the program"""
+        return True
+
+    def do_EOF(self, args):
+        """EOF signal that exits the program when Ctrl-D is invoked"""
+        print()
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
