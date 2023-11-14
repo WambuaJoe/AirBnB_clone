e143a7a32170a0e7140b5ee187dd20f8ec6b20d8
WambuaJoe <kaytujohn254@gmail.com>
2023-11-14 20:09:08 +0300

    implement emptyline method

--- console.py
+++ console.py
@@ -16,6 +16,10 @@ class HBNBCommand(cmd.Cmd):
         print("")
         return True
 
+    def emptyline(self):
+        """overwrite emptyline method"""
+        return False
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
