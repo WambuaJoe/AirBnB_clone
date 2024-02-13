e9f8f6e70491e27aedd50d145444bd94f524234a
WambuaJoe <kaytujohn254@gmail.com>
2024-02-13 16:38:57 +0300

    create BaseModel class that defines all common attributes & methods

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,50 @@
+#!/usr/bin/env python3
+"""base class for all models"""
 
+
+from datetime import datetime
+import uuid
+import models
+
+
+class BaseModel:
+    """Class Basemodel"""
+    instances = {}
+
+    def __init__(self, *args, **kwargs):
+        """constractor"""
+        if len(kwargs) == 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            self.__class__.instances[self.id] = self
+        else:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        if models.storage:
+            models.storage.new(self)
+
+    def save(self):
+        """updates the public instance attribute updated_at
+        with the current datetime"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def __str__(self):
+        """returns a string representation of a BaseModel class"""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__,
+            self.id,
+            self.__dict__
+        )
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__"""
+        my_dict = self.__dict__.copy()
+        my_dict['__class__'] = self.__class__.__name__
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        return my_dict
