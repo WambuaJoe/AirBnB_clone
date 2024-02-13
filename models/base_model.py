bf4dd8c3e8b9624d691f046822a67a9318152744
WambuaJoe <kaytujohn254@gmail.com>
2024-02-13 16:27:41 +0300

    code update - fix pycodestyle errors (base_model)

--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-"""Define a Base Class for all models
-    in the AirBnB clone
-"""
+"""module defines a base class for all models in the AirBnB clone"""
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """BaseModel class - all other classes inherit
-        from it
-    """
+    """Base model class for all other classes to inherit from"""
     def __init__(self, *args, **kwargs):
-        """constructor method that initializes attributes
-        """
+        """Constructor method to initialize attr"""
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
@@ -28,19 +23,20 @@ class BaseModel:
             self.__dict__.update(kwargs)
 
     def __str__(self):
-        """return string representation of an instance"""
-        cls = (str(type(self)).split('.')[-1].split('\'')[0])
+        """Return a string representation of instance"""
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
-        """update public instance attr updated_at with
-            current datetime"""
+        """Updates public instance attribute updated_at
+          with current datetime
+        """
         from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """returns dict containing all key:value pairs
+        """Returns dict containing all keys/values
         of __dict__ of instance"""
         dictionary = {}
         dictionary.update(self.__dict__)
