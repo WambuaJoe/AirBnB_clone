af2f0d95da53542bd8500700b1ea7bdd082675d3
WambuaJoe <kaytujohn254@gmail.com>
2024-02-13 16:46:23 +0300

    create BaseModel class

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Reloads JSON storage file"""
-from models.engine.file_storage import FileStorage
-
-
-storage = FileStorage()
-storage.reload()
-__all__ = ["base_model", "user", "state", "city", "amenity", "place", "review"]
--- models/base_model.py
+++ models/base_model.py
@@ -1,51 +1,51 @@
 #!/usr/bin/python3
-"""BaseModel Module"""
-import models
+"""
+Contains class BaseModel
+"""
+
 from datetime import datetime
+import models
 import uuid
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
-    """
-    Class BaseModel defines all common attributes/methods for other classes
-    """
+    """The BaseModel class from which future classes will be derived"""
 
     def __init__(self, *args, **kwargs):
-        """ Initializes the class """
-        tformat = "%Y-%m-%dT%H:%M:%S.%f"
+        """Initialization of the base model"""
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, tformat))
-                elif key != "__class__":
+                if key != "__class__":
                     setattr(self, key, value)
+            if hasattr(self, "created_at") and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            if hasattr(self, "updated_at") and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
+            models.storage.save()
 
     def __str__(self):
-        """
-        Returns an informal string representation of a BaseModel instance
-        [<class name>] (<self.id>) <self.__dict__>
-        """
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
-
-    def to_dict(self):
-        """
-        Returns a dictionary with all keys/values of __dict__ of the instance
-        """
-        inst_dict = self.__dict__.copy()
-        inst_dict["__class__"] = self.__class__.__name__
-        inst_dict["created_at"] = self.created_at.isoformat()
-        inst_dict["updated_at"] = self.updated_at.isoformat()
-        return inst_dict
+        """String representation of the BaseModel class"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
 
     def save(self):
-        """
-        Updates public instance attribute updated_at with current datetime
-        """
+        """updates the attribute 'updated_at' with the current datetime"""
         self.updated_at = datetime.now()
         models.storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of the instance"""
+        new_dict = self.__dict__.copy()
+        if "created_at" in new_dict:
+            new_dict["created_at"] = new_dict["created_at"].strftime(time)
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
